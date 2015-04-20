da6191fdb5c711d0bdbee59dc0d569ec4c48d97a
Александр Манолов <manolov@pyxis.ripcm.com>
2015-04-20 13:25:53 +0300

    gbf -> gb dir

--- scripts/runProkkaOrtho.sh
+++ scripts/runProkkaOrtho.sh
@@ -5,11 +5,11 @@ echo "genome fastas:" >> log; find ../fasta -name "*.fasta" | wc -l >> log
 bash helpers/prokkaloop.sh
 echo "prokka built gbf-s: " >> log; find ../prokka -name "*.gbf" | wc -l >> log
 #
-mkdir -p ../gbf
-find ../prokka -name "*.gbf" -exec cp {} ../gbf/ \;
+mkdir -p ../gb
+find ../prokka -name "*.gbf" -exec cp {} ../gb/ \;
 #
 mkdir -p ../prokka/faa
-for i in ../gbf/*.gbf
+for i in ../gb/*.gb*
 do
 	name=$(basename "$i"); name="${name%.*}"
 	perl helpers/GB2faa.pl $i  > ../prokka/faa/$name.fasta
@@ -21,5 +21,5 @@ cp ../prokka/faa/* ../faa
 # 2. ORTHO
 bash -x runOrtho.sh
 mkdir -p ../ortho
-mv blast.out goodProteins.* groups.txt mclInput named_groups.txt pairs pairs.log poorProteins.fasta simseq.txt ortho_out ../ortho
+mv blast.out goodProteins.* groups.txt mclInput named_groups.txt pairs pairs.log poorProteins.fasta simseq.txt ortho_table.txt ../ortho
 
