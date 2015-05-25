4bcedf5699f2e273fc2a42790439e7466b7842fb
Александр Манолов <manolov@pyxis.ripcm.com>
2015-05-25 18:28:58 +0300

    generate omcl config file

--- scripts/helpers/createdb.pl
+++ scripts/helpers/createdb.pl
@@ -5,9 +5,14 @@ while (<F>){
 	if($_ =~ /User\s+=\s+"(\w*)"/ ) 	{ $user = $1 };
 	if($_ =~ /Password\s+=\s+"(\w*)"/ )	{ $password = $1 };
 	if($_ =~ /Host\s+=\s+"([\w\.]*)"/ )	{ $host = $1 };
+	if($_ =~ /Port\s+=\s+"([\w\.]*)"/ )	{ $port = $1 };
 }
 
 print "\nwill create $DBname on $host\n";
 `mysql -u $user -p$password -e 'drop database if exists $DBname' --host $host`;
 `mysql -u $user -p$password -e 'create database $DBname' --host $host`; 
 
+# make config file for OMCL
+open C, "<helpers/db.config_fish";
+my $fish = do { local $/; <C> };
+eval $fish;
