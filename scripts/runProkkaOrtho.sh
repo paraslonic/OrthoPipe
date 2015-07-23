48d44226f440eb61efe2b60761d791e54e75b1f5
Александр Манолов <manolov@pyxis.ripcm.com>
2015-07-23 12:31:06 +0300

    names bug

--- scripts/runProkkaOrtho.sh
+++ scripts/runProkkaOrtho.sh
@@ -10,9 +10,14 @@ find ../prokka -name "*.gb*" -exec cp {} ../gb/ \;
 #
 echo "genebank files in gb dir: " >> log; find ../gb/ -name "*.gb*" | wc -l >> log
 mkdir -p ../prokka/faa
+
 for i in ../gb/*.gb*
 do
-	perl helpers/normalizeNames.pl $i
+        perl helpers/normalizeNames.pl $i
+done
+
+for i in ../gb/*.gb*
+do
 	name=$(basename "$i"); name="${name%.*}"
 	echo "$name"
 	perl helpers/GB2faa.pl $i  > ../prokka/faa/$name.fasta
